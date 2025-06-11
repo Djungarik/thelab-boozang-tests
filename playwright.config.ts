cbbb2ef3604e7754b6440a9c2a8714bdc4e7b85c
djungarik <berezhnoy.tim@gmail.com>
2025-06-11 16:05:21 +0300

    Clean up  Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,57 +1,27 @@
 import { EyesFixture } from "@applitools/eyes-playwright/fixture";
 import { defineConfig, devices } from "@playwright/test";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig<EyesFixture>({
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Configuration for Eyes VisualAI */
     screenshot: "on-first-failure",
     eyesConfig: {
-      /* The following and other configuration parameters are documented at: https://applitools.com/tutorials/playwright/api/overview */
       apiKey: process.env.APPLITOOLS_API_KEY,
-      // serverUrl: 'https://eyes.applitools.com',
-
-      // failTestsOnDiff: false,
-      // appName: 'My App',
-      // matchLevel: 'Strict',
-      // batch: { name: 'My Batch' },
-      // proxy: {url: 'http://127.0.0.1:8888'},
-      // stitchMode: 'CSS',
-      // matchTimeout: 0,
-      // waitBeforeScreenshots: 50,
-      // saveNewTests: true,
     },
 
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://thelab.boozang.com",
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
@@ -67,32 +37,5 @@ export default defineConfig<EyesFixture>({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
